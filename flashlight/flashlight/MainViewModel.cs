5054b71e7c5681be624bea0e4804d2046c80ebe4
rikupin1105 <41769991+rikupin1105@users.noreply.github.com>
2020-06-26 23:21:54 +0900

    プロジェクト ファイルを追加します。

---
+++ flashlight/flashlight/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace flashlight
+{
+    public class MainViewModel
+    {
+        public ICommand LightOnCommand { get; }
+        public ICommand LightOffCommand { get; }
+        public MainViewModel()
+        {
+            LightOnCommand = new Command(LightOn);
+            LightOffCommand = new Command(LightOff);
+        }
+        public bool LightStatus { get; set; } = false;
+        async void LightOn()
+        {
+            await Flashlight.TurnOnAsync();
+        }
+        async void LightOff()
+        {
+            await Flashlight.TurnOffAsync();
+        }
+    }
+}
